e56f3c5fea99e7f32295e7bc947fe626f19e49f2
root <root@vultr.guest>
2019-04-28 13:49:12 +0000

    install test

--- install_GoParse.sh
+++ install_GoParse.sh
@@ -4,4 +4,4 @@ yum update
 yum install git docker
 service docker start
 git clone https://github.com/Nicapoet/GoChessParse.git
-docker run -it --name=container_goparse -v ${PWD}/GoChessParse:/root/GoChessParse -p 8080:8080 nicapoet/go_count_img:latest /root/GoChessParse/start.sh 
+docker run -d --name=container_goparse -v ${PWD}/GoChessParse:/root/GoChessParse -p 8080:8080 nicapoet/go_count_img:latest /root/GoChessParse/start.sh 
