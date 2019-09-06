77e32813546f7390db85ccd2d13c82d8e8a5129c
nicapoet <nicapoet@outlook.com>
2019-09-06 22:42:50 +0800

    添加readme中的图片

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from src.WebInterface import *
 from src.ChessBoardParse import *
 if __name__ == '__main__':
     parser = ChessBoardParse('models/frozen_inference_graph.pb', 'models/go.pbtxt')
-    parser.output_matrix('upload/1.jpg')
+    parser.output_matrix('docs/srcImage.jpg')
     cv2.waitKey()
     #  app.run(host='0.0.0.0', port='8080', debug=True)
