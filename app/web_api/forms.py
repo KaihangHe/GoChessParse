c12b8b6f5daf3b2d8baaab03e62cce7e31c50ea3
nicapoet <nicapoet@outlook.com>
2019-09-11 08:57:08 +0800

    添加文件上传功能

--- app/web_api/forms.py
+++ app/web_api/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import DateField,FileField,SubmitField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileRequired,FileAllowed
 
 class ImageForm(FlaskForm):
-    image=FileField('',validators=[DataRequired()])
+    image=FileField('',validators=[FileRequired(),FileAllowed(['jpg','png','jpeg'])])
     submit=SubmitField('Submit')
--- app/web_api/views.py
+++ app/web_api/views.py
@@ -1,16 +1,16 @@
 from flask import render_template
 from . import upload_blueprint
 from .forms import ImageForm
-
+import os
 @upload_blueprint.route('/',methods=['GET','POST'])
 def upload():
     '''
-
     :return:
     '''
     form=ImageForm()
     if form.validate_on_submit():
-        image=form.image.data
-        print(image)
+        image_file=form.image.data
+        image_file.save(os.path.abspath(os.path.dirname(__file__))+"/../../upload/1.jpg")
+        print(image_file)
     #todo：调用cv_parse
     return render_template('upload.html',form=form)
